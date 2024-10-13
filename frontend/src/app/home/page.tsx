ba64316109037528e5fafab22f81ac9676048778
davidgamingvn <davidarthur260702@gmail.com>
2024-10-13 01:09:55 -0700

    mock data for weekly calendar view

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,52 +1,13 @@
 import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import Navbar from "~/components/NavBar";
 import { getSession } from "@auth0/nextjs-auth0";
+import WeeklyCalendar from "~/components/WeeklyCalendar";
+import { Suspense } from "react";
+import { Spinner } from "~/components/ui/spinner";
 
 export default async function HomeworkScheduler() {
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
   const session = await getSession();
   const user = session?.user;
-
-  const constraints = [
-    {
-      name: "sleep",
-      days: [0, 1, 2, 3, 4, 5, 6], // Days 0-6 represent Sunday to Saturday (entire week)
-      from: "22:00",
-      to: "06:00",
-    },
-    {
-      name: "workout",
-      days: [1, 3, 5], // Days 1, 3, 5 represent Monday, Wednesday, Friday
-      from: "07:00",
-      to: "08:00",
-    },
-    {
-      name: "study",
-      days: [2, 4], // Days 2, 4 represent Tuesday and Thursday
-      from: "16:00",
-      to: "18:00",
-    },
-  ];
-
-  const mockSchedule = days.map((day, index) => {
-    const dayConstraints = constraints
-      .filter((constraint) => constraint.days.includes(index))
-      .map((constraint) => ({
-        name: constraint.name,
-        from: constraint.from,
-        to: constraint.to,
-      }));
-    return { day, constraints: dayConstraints };
-  });
 
   return (
     <div className="min-h-screen bg-[#FFFBEB] dark:bg-slate-700">
@@ -59,24 +20,9 @@ export default async function HomeworkScheduler() {
             week:
           </CardHeader>
           <CardContent>
-            <div className="mt-4 grid grid-cols-7 gap-4">
-              {mockSchedule.map(({ day, constraints }) => (
-                <div key={day} className="text-center font-semibold">
-                  <div>{day}</div>
-                  {constraints.map((constraint, idx) => (
-                    <div
-                      key={idx}
-                      className="mt-2 rounded-md border border-gray-200 p-2"
-                    >
-                      <div className="font-bold">{constraint.name}</div>
-                      <div>
-                        {constraint.from} - {constraint.to}
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </div>
+            <Suspense fallback={<Spinner />}>
+              <WeeklyCalendar />
+            </Suspense>
           </CardContent>
         </Card>
       </main>
