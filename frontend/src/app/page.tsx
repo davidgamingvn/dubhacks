0d38a5c6a50561d0d7276325d1908962fd5c20fd
Quashnock <awq@uw.edu>
2024-10-12 17:58:23 -0700

    Changed up stylling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { Itim } from "next/font/google";
 import { Afacad } from "next/font/google";
-
-export const itim = Itim({
-  subsets: ["latin"],
-  weight: "400",
-  display: "swap",
-});
 
 export const afacad = Afacad({
   subsets: ["latin"],
-  weight: "400",
+  weight: ["400", "500"],
   display: "swap",
 });
 
 export default function HomePage() {
   return (
     <main className="flex h-[100vh] w-[100vw] flex-row">
-      <section className="flex h-full w-1/2 flex-col justify-between bg-[#FFF5B2] p-[5em]">
+      <section className="flex h-full w-1/2 flex-col justify-between bg-[#FAF17C] px-[4em] py-[4em] text-[#1E]">
         <div role="presentation">
-          <h1 className={`mb-10 text-7xl ${itim.className}`}>Scheduler</h1>
-          <p className="text-lg">
+          <h1
+            className={`mb-10 size-28 text-8xl font-semibold ${afacad.className}`}
+          >
+            Scheduler
+          </h1>
+          <p className={`text-lg ${afacad.className}`}>
             The AI scheduler to take one thing off your mind
           </p>
         </div>
-        <button className="${itim.className} rounded-md bg-orange-400 p-5">
+        <button
+          className={`${afacad.className} rounded-md border border-black bg-yellow-50 p-5 hover:bg-slate-100`}
+        >
           Log In
         </button>
       </section>
-      <section className="flex h-full w-1/2 items-center justify-center bg-orange-100">
+      <section className="flex h-full w-1/2 items-center justify-center bg-yellow-50">
         <Image
           alt="landing page"
           src="/Assets/LandingPageSVG.svg"
